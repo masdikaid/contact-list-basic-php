e07a90019fb986a6451cdd6d7a22e8b2ec93ff14
masdika ID <masdikaid@gmail.com>
2021-02-21 18:43:56 +0700

    building test to addContact

---
+++ test/testAddContact.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . "/../logic/addContact.php";
+
+addContact("yeyen", "082279992455");
+addContact("dika", "082279992455");
+addContact("adi", "082279992455");
+addContact("setiawan", "082279992455");
+addContact("ambarwati", "082279992455");
+
+var_dump($CONTACT_LIST);
