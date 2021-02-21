b0f761b48f8d64c1424bb766dc3d29f52c2318ae
masdika ID <masdikaid@gmail.com>
2021-02-21 16:10:10 +0700

    initial file

---
+++ app.php
@@ -0,0 +1,14 @@
+<?php
+//import model
+require_once "model/contacts.php";
+// import logic
+require_once "logic/showContact.php";
+require_once "logic/addContact.php";
+require_once "logic/updateContact.php";
+require_once "logic/deleteContact.php";
+// import view
+require_once "view/showContacts.php";
+require_once "view/addContact.php";
+require_once "view/updateContact.php";
+require_once "view/deleteContact.php";
+
---
+++ model/contacts.php
@@ -0,0 +1,3 @@
+<?php
+
+$CONTACT_LIST = [];
