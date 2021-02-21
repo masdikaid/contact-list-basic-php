283e196315482118a8ed50937ebc2fb98800165e
masdika ID <masdikaid@gmail.com>
2021-02-21 18:51:16 +0700

    add validate to contact if given blank string

--- logic/addContact.php
+++ logic/addContact.php
@@ -4,10 +4,14 @@ require_once __DIR__ . "/../model/contacts.php";
 
 function addContact(string $name, string $number)
 {
-    global $CONTACT_LIST;
-    $index = sizeof($CONTACT_LIST) + 1;
-    $CONTACT_LIST[$index] = [
-        "name" => $name,
-        "number" => $number
-    ];
+    if (strlen($name) == 0 or strlen($number) == 0){
+        echo "name and number is required \n";
+    } else {
+        global $CONTACT_LIST;
+        $index = sizeof($CONTACT_LIST) + 1;
+        $CONTACT_LIST[$index] = [
+            "name" => $name,
+            "number" => $number
+        ];
+    };
 };
