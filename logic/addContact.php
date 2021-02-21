c291b3c77a219431756999bfd592762d9fb396e3
masdika ID <masdikaid@gmail.com>
2021-02-21 18:32:59 +0700

    build function add contact

--- logic/addContact.php
+++ logic/addContact.php
@@ -1,6 +1,13 @@
 <?php
 
-function addContact()
-{
+require_once __DIR__ . "../model/contacts.php";
 
+function addContact(string $name, string $number)
+{
+    global $CONTACT_LIST;
+    $index = sizeof($CONTACT_LIST) + 1;
+    $CONTACT_LIST[$index] = [
+        "name" => $name,
+        "number" => $number
+    ];
 };
