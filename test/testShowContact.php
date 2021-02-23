f033b00f79b943e7ce125b46a918e8d3834927ab
masdika ID <masdikaid@gmail.com>
2021-02-23 19:43:47 +0700

    test show contact logic

---
+++ test/testShowContact.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . "/../logic/addContact.php";
+require_once __DIR__ . "/../logic/showContact.php";
+
+
+addContact("yeyen", "082279992455");
+addContact("dika", "082279992455");
+addContact("adi", "082279992455");
+addContact("setiawan", "082279992455");
+addContact("ambarwati", "082279992455");
+
+showContact();
+
