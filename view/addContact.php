b8091afb562eb03d3e1aabb6e54dd09c93e312a8
masdika ID <masdikaid@gmail.com>
2021-02-23 19:44:37 +0700

    build view for contact list

--- view/addContact.php
+++ view/addContact.php
@@ -1,1 +1,23 @@
 <?php
+
+require_once __DIR__ . "/../helper/input.php";
+require_once __DIR__ . "/../logic/addContact.php";
+
+
+function viewAddContact(){
+    echo "=============ADD CONTACT=============\n";
+    $name = input("input name : (x to cancel)");
+    if ($name === "x"){
+        echo "add contact canceled";
+        return false;
+    };
+
+    $number = input("input number : (x to cancel)");
+    if ($name === "x"){
+        echo "add contact canceled";
+        return false;
+    };
+
+    return addContact($name, $number);
+};
+
