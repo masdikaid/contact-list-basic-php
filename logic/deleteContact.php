a42cb96c799e76fe535af7f05b936c07f9b0c511
masdika ID <masdikaid@gmail.com>
2021-02-23 19:42:39 +0700

    build logic to delete contact

--- logic/deleteContact.php
+++ logic/deleteContact.php
@@ -1,6 +1,22 @@
 <?php
 
-function deleteContact()
-{
+require_once __DIR__ . "/../model/contacts.php";
 
+function deleteContact(int $id)
+{
+    global $CONTACT_LIST;
+    if ($id == 0 or $id > sizeof($CONTACT_LIST)){
+        echo "id $id not found ...\n";
+        return false;
+    } else {
+        for ($i = $id; $i < sizeof($CONTACT_LIST); $i++)
+        {
+            $CONTACT_LIST[$i] = [
+                "name" => $CONTACT_LIST[$i+1]["name"],
+                "number" => $CONTACT_LIST[$i+1]["number"]
+            ];
+        };
+        unset($CONTACT_LIST[sizeof($CONTACT_LIST)]);
+        return true;
+    };
 };
