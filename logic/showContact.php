b1f19e5dc55bb196be540b668056766a0aa8c9a5
masdika ID <masdikaid@gmail.com>
2021-02-23 19:42:56 +0700

    build logic for showing contact list

--- logic/showContact.php
+++ logic/showContact.php
@@ -1,6 +1,16 @@
 <?php
 
+require_once __DIR__ . "/../model/contacts.php";
+
 function showContact()
 {
-
+    global $CONTACT_LIST;
+    foreach ($CONTACT_LIST as $i => $contact){
+        echo <<<CONTENT
+        =============== $i ===============
+        NAME \t : \t {$contact["name"]}
+        NUMBER \t : \t {$contact["number"]}
+        =================================\n\n
+        CONTENT;
+    };
 };
