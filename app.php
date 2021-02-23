e66388299adbc4d09635de212fca3c8eea6486d8
masdika ID <masdikaid@gmail.com>
2021-02-23 19:46:07 +0700

    joining all feature

--- app.php
+++ app.php
@@ -1,14 +1,45 @@
 <?php
+
+require_once __DIR__ . "/helper/input.php";
 //import model
-require_once "model/contacts.php";
-// import logic
-require_once "logic/showContact.php";
-require_once "logic/addContact.php";
-require_once "logic/updateContact.php";
-require_once "logic/deleteContact.php";
+require_once __DIR__ . "/model/contacts.php";
 // import view
-require_once "view/showContacts.php";
-require_once "view/addContact.php";
-require_once "view/updateContact.php";
-require_once "view/deleteContact.php";
+require_once __DIR__ . "/view/showContacts.php";
+require_once __DIR__ . "/view/addContact.php";
+require_once __DIR__ . "/view/updateContact.php";
+require_once __DIR__ . "/view/deleteContact.php";
+
+while (true) {
+    viewShowContact();
+    echo <<<MENU
+    menu :
+    \t1. add contact
+    \t2. update contact
+    \t3. delete contact
+    \t4. exit\n\n
+    MENU;
+
+    $choice = input("select : ");
+    if ($choice == 4){
+        break;
+    };
+    switch ($choice) {
+        case 1:
+            viewAddContact();
+            break;
+
+        case 2:
+            viewUpdateContact();
+            break;
+
+        case 3:
+            viewDeleteContact();
+            break;
+        
+        default:
+            echo "retype your coiche !!!";
+            break;
+    };
+};
+echo "SELESAI";
 
