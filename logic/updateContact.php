38357fc31e04d7dc874ee9b3cdd7f46e499e89c7
masdika ID <masdikaid@gmail.com>
2021-02-23 19:43:16 +0700

    build logic to update contact information

--- logic/updateContact.php
+++ logic/updateContact.php
@@ -1,6 +1,19 @@
 <?php
 
-function updateContact()
+function updateContact(int $id, string $name, string $number)
 {
-
+    global $CONTACT_LIST;
+    if (strlen($name) == 0 or strlen($number) == 0){
+        echo "id or name and number is required \n";
+        return false;
+    } else if ($id > sizeof($CONTACT_LIST) or $id == 0) {
+        echo "data not found \n";
+        return false;
+    } else {
+        $CONTACT_LIST[$id] = [
+            "name" => $name,
+            "number" => $number
+        ];
+        return true;
+    };
 };
