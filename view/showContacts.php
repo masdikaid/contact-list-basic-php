a5c941065501755a0afbbaf4d5a60955e8518370
masdika ID <masdikaid@gmail.com>
2021-02-23 19:45:14 +0700

    build view for showing contact list

--- view/showContacts.php
+++ view/showContacts.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once __DIR__ . "/../logic/showContact.php";
+
+function viewShowContact(){
+    echo "=============== MY CONTACT ===============\n\n\n";
+    showContact();
+    echo "=============== MY CONTACT ===============\n\n\n";
+};
+
